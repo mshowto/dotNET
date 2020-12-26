c0c2a540c7acd93d6354f04a00cf1c141abff1ed
Servan ORAL <44291875+ServanORAL@users.noreply.github.com>
2020-12-26 18:24:38 +0300

    Add files via upload
    Singleton Tasarım Deseni ile ilgili örnek

---
+++ SingletonExample/SingletonExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personManager=PersonManager.CreateSingleton();
+        }
+    }
+
+
+    class PersonManager
+    {
+        static PersonManager _personManager;
+        static object _locked = new object();
+        private PersonManager()
+        {
+
+        }
+
+        public static PersonManager CreateSingleton()
+        {
+            lock (_locked)
+            {
+                if (_personManager == null)
+                {
+                    _personManager = new PersonManager();
+                }
+            }
+
+            return _personManager;
+        }
+    }
+
+
+
+}
