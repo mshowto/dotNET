756e00608ee17a906135ad472cefc834928e00c4
Servan ORAL <44291875+ServanORAL@users.noreply.github.com>
2020-12-26 18:25:54 +0300

    Add files via upload
    ProtoType Tasarım Deseni İle İlgili Örnekler

---
+++ PrototypeExample/PrototypeExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MobilePhone mobilePhone = new MobilePhone { Brand = "Apple", Model = "IPhone" };
+
+            MobilePhone mobilePhone1 = (MobilePhone)mobilePhone.Clone();
+            mobilePhone1.Brand = "Samsung";
+            mobilePhone1.Model = "Galaxy";
+
+
+        }
+    }
+
+    public abstract class Phone
+    {
+        public abstract Phone Clone();
+
+        public string Brand { get; set; }
+    }
+
+    public class MobilePhone : Phone
+    {
+        public override Phone Clone()
+        {
+            return (Phone)MemberwiseClone();
+        }
+
+        public string Model { get; set; }
+    }
+}
